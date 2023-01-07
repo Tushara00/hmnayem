0e6d4524490c173c66333a661d6a3b89aa7164d5
Halima <trishaahamed300@gmail.com>
2023-01-07 11:57:58 -0800

    aded new method called greeting in java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main (string[] args){
-        system.out.printIn("vscode is Awesome")
+        system.out.printIn("vscode is Awesome");
+        greetings ("HM nayem");
+    }
+    public static void greetings (string name){
+        system.out.printIn("welcome" + name);
     }
 }
