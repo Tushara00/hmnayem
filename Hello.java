737a4dd6afd45f2b999563af45c4d80446c04f98
Halima <trishaahamed300@gmail.com>
2023-01-07 12:47:21 -0800

    change greeting method

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello {
     public static void main (string[] args){
         system.out.printIn("vscode is Awesome");
-        greetings ("HM nayem");
+        greetings ("git");
     }
     public static void greetings (string name){
-        system.out.printIn("welcome" + name);
+        system.out.printIn("bye bye" + name);
     }
 }
